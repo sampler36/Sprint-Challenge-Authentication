378d5abc59a3017b6ab7e880eda1720baaf98f61
Talent Antonio <antonio.talenty@gmail.com>
2019-04-12 13:48:22 +0100

    Bug fixes

--- auth/authenticate.js
+++ auth/authenticate.js
@@ -1,9 +1,9 @@
 const jwt = require('jsonwebtoken');
 const secrets = require('../config/secrets');
 
-const jwtKey =
-  process.env.JWT_SECRET ||
-  'add a .env file to root of project with the JWT_SECRET variable';
+// const jwt =
+//   process.env.JWT_SECRET ||
+//   'add a .env file to root of project with the JWT_SECRET variable';
 
 // quickly see what this file exports
 module.exports = {
@@ -30,7 +30,7 @@ function authenticate(req, res, next) {
   const token = req.get('Authorization');
 
   if (token) {
-    jwt.verify(token, jwtKey, (err, decoded) => {
+    jwt.verify(token, secrets.jwtSecret, (err, decoded) => {
       if (err) return res.status(401).json(err);
 
       req.decoded = decoded;
