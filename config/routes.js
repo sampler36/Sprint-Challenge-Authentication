a702b66b6d98f838362f2756fbf3919585208ad9
Talent Antonio <antonio.talenty@gmail.com>
2019-04-12 09:36:51 +0100

    Register implemented

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,6 @@
 const axios = require('axios');
 const bcrypt = require('bcrypt');
+const Users = require('../users/users-model');
 
 const { authenticate } = require('../auth/authenticate');
 
@@ -11,7 +12,17 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10); 
+  user.password = hash;
 
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 }
 
 function login(req, res) {
